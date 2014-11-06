2cb105dbef9bfc4719a1d50fb2aab646df8e6ac3
wkwok16 <wkwok16@punahou.edu>
2014-11-06 12:01:14 -1000

    Activity 3
    Changed statement.indexOf("") to findKeyword(statement, “”). Added the
    findKeyword Magpie API thing

--- Magpie.java
+++ Magpie.java
@@ -1,15 +1,3 @@
-/**
- * A program to carry on conversations with a human user.
- * This is the initial version that:  
- * <ul><li>
- *       Uses indexOf to find strings
- * </li><li>
- *       Handles responding to simple words and phrases 
- * </li></ul>
- * This version uses a nested if to handle default responses.
- * @author Laurie White
- * @version April 2012
- */
 public class Magpie
 {
   /**
@@ -31,28 +19,27 @@ public class Magpie
   public String getResponse(String statementIn)
   {
     String response = "";
-    String statement = statementIn.toLowerCase(); // ADDED IN ACTIVITY 2
-    if (statement.indexOf("no") >= 0)
+    String statement = statementIn.toLowerCase(); // Makes string to lower case
+    if (findKeyword(statement, "no") >= 0) //Changed statement.indexOf("") to findKeyword(statement, "")
     {
       response = "Why so negative?";
     }
-    else if (statement.indexOf("mother") >= 0
-               || statement.indexOf("father") >= 0
-               || statement.indexOf("sister") >= 0
-               || statement.indexOf("brother") >= 0)
+    else if (findKeyword(statement, "mother") >= 0 
+               || findKeyword(statement, "father") >= 0 
+               || findKeyword(statement, "sister") >= 0 
+               || findKeyword(statement, "brother") >= 0)
     {
       response = "Tell me more about your family.";
     }
-    ////////ACTIVITY 2///////////
-    else if(statement.indexOf("cat") >= 0
-              || statement.indexOf("dog") >= 0
-              || statement.indexOf("pet") >= 0
-              || statement.indexOf("fish") >= 0)
+    else if(findKeyword(statement, "cat") >= 0
+              || findKeyword(statement, "dog") >= 0
+              || findKeyword(statement, "pet") >= 0
+              || findKeyword(statement, "fish") >= 0)
     {
       response = "Tell me more about your pets.";
     }
-    else if(statement.indexOf("kiang") >= 0
-              || statement.indexOf("landgraf") >= 0)
+    else if(findKeyword(statement, "kiang") >= 0
+              || findKeyword(statement, "landgraf") >= 0)
     {
       response = "I've heard about that guy! He's a pretty smart and swell person!";
     }
@@ -60,20 +47,20 @@ public class Magpie
     {
       response = "Say somthing, please";
     }
-    else if(statement.indexOf("hungry") >= 0
-              || statement.indexOf("food") >= 0)
+    else if(findKeyword(statement, "hungry") >= 0
+              || findKeyword(statement, "food") >= 0)
     {
       response = "What foods do you enjoy?";
     }
-    else if(statement.indexOf("school") >= 0
-              || statement.indexOf("grades") >= 0
-              || statement.indexOf("gpa") >= 0)
+    else if(findKeyword(statement, "school") >= 0
+              || findKeyword(statement, "grades") >= 0
+              || findKeyword(statement, "gpa") >= 0)
     {
       response = "Sounds difficult. It'll all be worth it in the end, though!";
     }
-    else if(statement.indexOf("video games") >= 0
-              || statement.indexOf("games") >= 0
-              || statement.indexOf("controller") >= 0)
+    else if(findKeyword(statement, "video games") >= 0
+              || findKeyword(statement, "games") >= 0
+              || findKeyword(statement, "controller") >= 0)
     {
       response = "I enjoy playing lots of video games! I enjoy playing Osu! in particular!";
     }         
@@ -85,6 +72,44 @@ public class Magpie
     return response;
   }
   
+  private int findKeyword(String statement, String goal, int startPos)
+  {
+    String phrase = statement.trim();
+    // The only change to incorporate the startPos is in
+    // the line below
+    int psn = phrase.toLowerCase().indexOf(goal.toLowerCase(), startPos);
+    // Refinement--make sure the goal isn't part of a
+    // word
+    while (psn >= 0)
+    {
+      // Find the string of length 1 before and after
+      // the word
+      String before = " ", after = " ";
+      if (psn > 0)
+      {
+        before = phrase.substring(psn - 1, psn).toLowerCase();
+      }
+      if (psn + goal.length() < phrase.length())
+      {
+        after = phrase.substring(psn + goal.length(),psn + goal.length() + 1).toLowerCase();
+      }
+      // If before and after aren't letters, we've
+      // found the word
+      if (((before.compareTo("a") < 0) || (before.compareTo("z") > 0)) // before is not a letter
+            && ((after.compareTo("a") < 0) || (after.compareTo("z") > 0)))
+      {
+        return psn;
+      }
+      // The last position didn't work, so let's find
+      // the next, if there is one.
+      psn = phrase.indexOf(goal.toLowerCase(),psn + 1);
+    }
+    return -1;
+  }
+  private int findKeyword(String statement, String goal)
+  {
+    return findKeyword(statement, goal, 0);
+  }
   /**
    * Pick a default response to use if nothing else fits.
    * @return a non-committal string
